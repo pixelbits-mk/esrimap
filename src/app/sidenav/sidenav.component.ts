8f4b1c6ab2363df3eaf91264ac92703152f2cd9d
Mike Kang <kmike@vyana.io>
2019-12-07 11:28:23 -0500

    updated

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -11,5 +11,7 @@ export class SidenavComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  onHide() {
+    document.getElementById('sidenav').classList.remove('visible');
+  }
 }
